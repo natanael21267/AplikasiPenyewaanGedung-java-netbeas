cb0fa9a33a096a0c4648a255d17ea45b55bb6b38
natanael21267 <154845943+natanael21267@users.noreply.github.com>
2024-12-27 02:12:48 +0700

    Add files via upload

---
+++ Koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Admin
+ */
+public class Koneksi {
+    private static Connection conn;
+    
+    public static Connection getConnection(){
+        if (conn == null) {
+            try {
+                String url = "jdbc:mysql://localhost/gedung";
+                String user = "root";
+                String pass = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            }
+            catch (Exception e) {
+                Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return conn;
+    }
+}
+    
